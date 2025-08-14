150b8ea405873dedeb4489188ce554770c6e680a
Jurian Onderwater <jurian.onderwater247@gmail.com>
2025-08-14 21:34:33 +0200

    change map style

--- static/js/map.js
+++ static/js/map.js
@@ -31,10 +31,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- 1. Initialise Viewers ---
     const map = L.map('map').setView([20, 0], 2);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    }).addTo(map);
 
     const viewer = new Cesium.Viewer('cesium-container', {
-        // imageryProvider: new Cesium.UrlTemplateImageryProvider('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'),
+        imageryProvider: new Cesium.UrlTemplateImageryProvider({
+            url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c', 'd']
+        }),
         animation: false, timeline: false, geocoder: false, homeButton: false, 
         sceneModePicker: false, baseLayerPicker: false, navigationHelpButton: false, 
         infoBox: false, selectionIndicator: false, fullscreenButton: false,
